b49a395037dbb26bb7fb74b0ce6b0039fc427896
puimekster <hoanhao18102000@gmail.com>
2022-01-12 21:46:51 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import GoogleLogin from "react-google-login";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [loginData, setLoginData] = useState(
+		localStorage.getItem("loginData")
+			? JSON.parse(localStorage.getItem("loginData"))
+			: null
+	);
+	const handleLogin = async (googleData) => {
+		const res = await fetch("/api/google-login", {
+			method: "POST",
+			body: JSON.stringify({
+				token: googleData.tokenId,
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		const data = await res.json();
+		setLoginData(data);
+		localStorage.setItem("loginData", JSON.stringify(data));
+	};
+
+	const handleFailure = (resuilt) => {
+		console.log(resuilt);
+	};
+	const handleLogout = () => {
+		localStorage.removeItem("loginData");
+		setLoginData(null);
+	};
+	return (
+		<div className="App">
+			<h1>Login Google</h1>
+			<div>
+				{loginData ? (
+					<div>
+						<h1>My Name: {loginData.name}</h1>
+						<h3>Logged in as {loginData.email}</h3>
+						<button onClick={handleLogout}>LogOut</button>
+					</div>
+				) : (
+					<GoogleLogin
+						clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+						buttonText="Log in with Google"
+						onSuccess={handleLogin}
+						onFailure={handleFailure}
+						cookiePolicy={"single_host_origin"}
+					></GoogleLogin>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default App;
